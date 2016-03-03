57917fbaa5b3e1f791c9a66a5dd13758ea16eca6
Kristoffer Finckenhagen <kristoffer.finckenhagen@gmail.com>
2016-03-03 19:27:24 +0100

    added files for the raspberry Pi

---
+++ Pi/dbtest.py
@@ -0,0 +1,54 @@
+import MySQLdb
+import time
+import os
+import glob
+
+
+#Kode som leser temperatur via sensoren paa Raspberry Pi.
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c
+
+
+#db = MySQLdb.connect(host="mysql.stud.ntnu.no",             # host
+#                     user="roberei_hotdog",                 # username
+#                     passwd="pekka",                        # password
+#                     db="roberei_hotdog_db")                # databasename
+
+#Cursorprosjekt lets you perform queries
+#cur = db.cursor()
+
+# SQL & shit here
+temperature = 0.0
+device_on = True
+while(device_on):
+    print(read_temp())
+    temperature = read_temp()
+    #cur.execute("INSERT INTO DataLinje (temperatur, raspberry_lopeNr) VALUES ('%s', '%s')",(temperature, 1))
+    time.sleep(5)
+#    db.commit()
+
+
+
+#db.close()
